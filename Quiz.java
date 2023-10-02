fb703d63a6ed5ca4ef9baa5511a6771633748817
Ayushgupta015 <132714587+Ayushgupta015@users.noreply.github.com>
2023-10-02 22:36:58 +0530

    Add files via upload

---
+++ Quiz.java
@@ -0,0 +1,13 @@
+public class Quiz {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        int calculate = (a*b)/(a-b);
+        System.out.println(calculate);
+    }
+    
+}
+//QUIZ
+/* 
+ (a*b)/(a-b) , a=10, b=5
+ */
